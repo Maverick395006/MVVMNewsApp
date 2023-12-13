a4ac1e04b6c7920d8ed0d6a5d857cfce682c69e2
Piyush <piyush.radadiya288@gmail.com>
2023-12-13 12:05:04 +0530

    12 Handling Pagination with Retrofit

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -6,6 +6,7 @@ class Constants {
         const val API_KEY = "50b4b038c5b24576835cd16fa6417f6e"
         const val BASE_URL = "https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 
 }
